2a0c988131200b186392d3aa78fde112d476205a
avezqureshi14 <aveznqureshi14@gmail.com>
2024-02-24 13:51:43 +0530

    fix: server deployed

--- src/main.js
+++ src/main.js
@@ -27,10 +27,109 @@ const runActor = async () => {
               const data = await gplay.app({ appId: app.appId });
               console.log(data);
               const title = data?.title;
-              await axios.post(
-                "https://avez-blog-2023-end.onrender.com/apps",
-                {title}
-              );
+              const description = data?.description;
+              const descriptionHTML = data?.descriptionHTML;
+              const summary = data?.summary;
+              const installs = data?.installs;
+              const minInstalls = data?.minInstalls;
+              const maxInstalls = data?.maxInstalls;
+              const score = data?.score;
+              const scoreText = data?.scoreText;
+              const ratings = data?.ratings;
+              const reviews = data?.reviews;
+              const histogram = data?.histogram;
+              const price = data?.price;
+              const free = data?.free;
+              const currency = data?.currency;
+              const priceText = data?.priceText;
+              const available = data?.available;
+              const offersIAP = data?.offersIAP;
+              const IAPRange = data?.IAPRange;
+              const androidVersion = data?.androidVersion;
+              const androidVersionText = data?.androidVersionText;
+              const androidMaxVersion = data?.androidMaxVersion;
+              const developer = data?.developer;
+              const developerId = data?.developerId;
+              const developerEmail = data?.developerEmail;
+              const developerWebsite = data?.developerWebsite;
+              const developerAddress = data?.developerAddress;
+              const privacyPolicy = data?.privacyPolicy;
+              const developerInternalID = data?.developerInternalID;
+              const genre = data?.genre;
+              const genreId = data?.genreId;
+              const categories = data?.categories;
+              const icon = data?.icon;
+              const headerImage = data?.headerImage;
+              const screenshots = data?.screenshots;
+              const video = data?.video;
+              const videoImage = data?.videoImage;
+              const previewVideo = data?.previewVideo;
+              const contentRating = data?.contentRating;
+              const contentRatingDescription = data?.contentRatingDescription;
+              const adSupported = data?.adSupported;
+              const released = data?.released;
+              const updated = data?.updated;
+              const version = data?.version;
+              const recentChanges = data?.recentChanges;
+              const comments = data?.comments;
+              const preregister = data?.preregister;
+              const earlyAccessEnabled = data?.earlyAccessEnabled;
+              const isAvailableInPlayPass = data?.isAvailableInPlayPass;
+              const appId = data?.appId;
+              const url = data?.url;
+              await axios.post("https://avez-blog-2023-end.onrender.com/apps", {
+                title,
+                description,
+                descriptionHTML,
+                summary,
+                installs,
+                minInstalls,
+                maxInstalls,
+                score,
+                scoreText,
+                ratings,
+                reviews,
+                histogram,
+                price,
+                free,
+                currency,
+                priceText,
+                available,
+                offersIAP,
+                IAPRange,
+                androidVersion,
+                androidVersionText,
+                androidMaxVersion,
+                developer,
+                developerId,
+                developerEmail,
+                developerWebsite,
+                developerAddress,
+                privacyPolicy,
+                developerInternalID,
+                genre,
+                genreId,
+                categories,
+                icon,
+                headerImage,
+                screenshots,
+                video,
+                videoImage,
+                previewVideo,
+                contentRating,
+                contentRatingDescription,
+                adSupported,
+                released,
+                updated,
+                version,
+                recentChanges,
+                comments,
+                preregister,
+                earlyAccessEnabled,
+                isAvailableInPlayPass,
+                appId,
+                url
+              });
             } catch (error) {
               console.log(error);
             }
