6e8ea773443a9f7fd18489ca9711aa0c2deac9a5
avezqureshi14 <aveznqureshi14@gmail.com>
2024-02-26 17:31:09 +0530

    controller updated

--- src/main.js
+++ src/main.js
@@ -6,17 +6,7 @@ import {
 } from "./constants/actionTypes.js";
 import { logError } from "./utility/logError.js";
 import { ScraperFactory } from "./scrappers/scrapper-factory.js";
-import {
-  addReviewToDatabase,
-  addSupportedDeviceToDatabase,
-  createApplicationInDatabase,
-  createDeveloperInDatabase,
-  getCategoryFromDatabase,
-  getCollectionFromDatabase,
-  getDeveloperFromDatabase,
-  getPlatformFromDatabase,
-  updateApplication,
-} from "./utility/supabase.js";
+import { processDB } from "./utility/processDB.js";
 
 const runActor = async () => {
   try {
@@ -28,98 +18,7 @@ const runActor = async () => {
     switch (action) {
       case LIST_APPS: {
         const apps = await storeInstance.listApps(input);
-        const { selectedCollection, selectedCategory, platform, num } = input;
-        await Promise.all(
-          apps?.slice(0, input.limit)?.map(async (app) => {
-            try {
-              const data = await storeInstance.getAppDetails({
-                appId: app?.id,
-              });
-
-              const category = await getCategoryFromDatabase(selectedCategory);
-              const collection = await getCollectionFromDatabase(
-                selectedCollection
-              );
-              let developer = await getDeveloperFromDatabase(data.developerId);
-
-              if (!developer) {
-                const developerData = {
-                  developer_identifier: data.developerId,
-                  name: data.developer,
-                  developer_url: data.developerUrl,
-                  developer_website: data.developerWebsite,
-                };
-                developer = await createDeveloperInDatabase(developerData);
-              }
-
-              const platformId = await getPlatformFromDatabase(platform);
-              const reviewData = {
-                score: data?.score,
-                current_version_score: data?.currentVersionScore,
-                current_version_reviews: data?.currentVersionReviews,
-              };
-              const supportedDeviceData = {
-                device_name: data?.supportedDevices,
-              };
-
-              const application = await createApplicationInDatabase({
-                application_identifier: data?.id,
-                title: data?.title,
-                url: data?.url,
-                description: data?.description,
-                icon: data?.icon,
-                content_rating: data?.contentRating,
-                languages: data?.languages,
-                size: data?.size,
-                required_os_version: data?.requiredOsVersion,
-                released: data?.released,
-                updated: data?.updated,
-                release_notes: data?.releaseNotes,
-                version: data?.version,
-                price: data?.price,
-                currency: data?.currency,
-                free: data?.free,
-                has_iap: null,
-                has_ads: null,
-                installs: null,
-                installs_range: null,
-                iap_range: null,
-                developer_identifier: developer,
-                screenshot_identifier: null,
-                supported_device_identifier: null,
-                review_identifier: null,
-                platform_identifier: platformId,
-                category_identifier: category,
-                collection_identifier: collection,
-              });
-              const applicationIdentifier = application?.application_identifier;
-
-              // Step 3: Use the application identifier to create related records
-              const review_identifier = await addReviewToDatabase(
-                applicationIdentifier,
-                reviewData
-              );
-              const supported_device_identifier =
-                await addSupportedDeviceToDatabase(
-                  applicationIdentifier,
-                  supportedDeviceData
-                );
-
-              await updateApplication(
-                applicationIdentifier,
-                review_identifier,
-                supported_device_identifier
-              );
-            } catch (error) {
-              console.error(
-                `Error processing app details for appId: ${app?.id}`,
-                error
-              );
-              // Handle the error as needed
-            }
-          })
-        );
-
+        await processDB(apps, input, storeInstance);
         await Actor.pushData(apps.slice(0, input.limit));
         break;
       }
