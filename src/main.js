ff0b0859c61f0d9fd1bb591f4a8e3f33dc6006f4
avezqureshi <aveznqureshi14@gmail.com>
2024-03-05 18:27:24 +0530

    fix : issuing fixing of reviews

--- src/main.js
+++ src/main.js
@@ -64,7 +64,7 @@ const runActor = async () => {
               let reviews = await storeInstance.getReviews(input);
               array.push(reviews);
               array.map((item)=>{
-                  console.log(item?.text);
+                  console.log(item);
               })
               const reviewData = {
 
