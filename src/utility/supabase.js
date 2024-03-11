b83cd334f389fd60267017040cf03715977dc96b
avezqureshi <aveznqureshi14@gmail.com>
2024-03-11 11:29:43 +0530

    feat: working on readability

--- src/utility/supabase.js
+++ src/utility/supabase.js
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
+import { logError } from "./logError";
 const supabaseUrl = "https://jjvixvxxvjucmnddztii.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impqdml4dnh4dmp1Y21uZGR6dGlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc4MzY4MzEsImV4cCI6MjAyMzQxMjgzMX0.fiSiIflKB14U3yJeZ83-nwksOdm58TnlDWcaquHAnVQ";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 import axios from "axios";
-import { logError } from "./logError";
 
 const handleSupabaseResponse = (response, message) => {
   if (response.error) {
